485143e1b47cf087e5a90b8ee84b44721f861d2d
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-05-11 07:56:59 +0900

    write some test (no enough yet)

--- spec/giic_spec.rb
+++ spec/giic_spec.rb
@@ -1,9 +1,42 @@
-require File.dirname(__FILE__) + '/../giic'
+require File.dirname(__FILE__) + '/spec_helper'
 
-describe Giic do
-  # TODO; Need to write a test
-  it 'should be true' do
-    true.should be_true
+# TODO: need more test code
+class Giic
+
+  describe Giic do
+    %w[ search list show ].each do |meth|
+      instance_eval <<-EOS
+        it "has #{meth} method" do
+          Giic.instance_methods.should be_include('#{meth}')
+        end
+      EOS
+    end
+
+    describe User do
+      %w[
+        open close reopen edit label
+        comment add_label remove_label
+      ].each do |meth|
+        instance_eval <<-EOS
+          it "has #{meth} method" do
+            User.instance_methods.should be_include('#{meth}')
+          end
+        EOS
+      end
+    end
+
+    describe Core do
+      %w[
+        search list show open close
+        reopen edit label comment
+      ].each do |meth|
+        instance_eval <<-EOS
+          it "ihas #{meth} method" do
+            Core.should respond_to :#{meth}
+          end
+        EOS
+      end
+    end
   end
 end
 
